fc892a5e8676807702ca96eae86af00f73d3cc61
krishnakant8010 <kantkrishna2016@gmail.com>
2023-02-07 00:12:26 +0530

    Update start your project page for form validation with valid data

--- BenthonLabsApplication/src/test/java/com/benthonLabs/BenthonLabsApplication/Website_OpenOn_Browser.java
+++ BenthonLabsApplication/src/test/java/com/benthonLabs/BenthonLabsApplication/Website_OpenOn_Browser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -17,15 +18,14 @@ public class Website_OpenOn_Browser {
 		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.get("https://www.benthonlabs.com/");
 		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 	}
 	
-	@Test
-	public void Verify_Website_Open() {
-		driver.get("https://www.benthonlabs.com/");
-		String ExpectedTitle = "Benthon Labs – Cloud-Based DevOps Services and Solutions";
-		String ActualTitle = driver.getTitle();
-		Assert.assertEquals(ExpectedTitle, ActualTitle);
+	
+	@AfterTest
+	public void Closed_Opened_Browser() {
+			driver.close();
 	}
 	
 	
